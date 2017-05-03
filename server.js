a9f729338645090be21183c2b0cd8fa0bccb0723
nukeline <nukeline@protonmail.com>
2017-05-03 20:46:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-var port = var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
